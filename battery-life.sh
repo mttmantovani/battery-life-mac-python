e0b1e10757cb7aab5f516fd62a19c40ee60b9438
Mattia Mantovani <mattia.mantovani@gmail.com>
2021-02-25 17:15:25 +0100

    updated data

--- battery-life.sh
+++ battery-life.sh
@@ -6,7 +6,7 @@
 CYCLES=$(/usr/sbin/system_profiler SPPowerDataType | grep "Cycle Count" | awk '{print $3}')
 CAPACITY=$(/usr/sbin/system_profiler SPPowerDataType | grep "Full Charge Capacity" | awk '{print $5}')
 DESIGN_CAPACITY=$(/usr/sbin/ioreg -l -w0 | grep "\"DesignCapacity\" =" | awk '{print $5}')
-PYTHON=/usr/local/bin/python3
+PYTHON=python
 WD=$DEV/battery-life
 OUTPUT=$WD/data.csv
 
